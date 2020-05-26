94c9f469e7ce3ea0954d8b7aa336a48d6e22d486
Art Balahadia <artbalahadia@Arts-MacBook-Pro.local>
2020-05-25 21:41:59 -0500

    fixing API on weather.js

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ class Weather {
 
     // Fetch from API
     async getWeather(){
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}`)
     
         const responseData = await response.json();
         return responseData;
